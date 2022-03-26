90a0b26f5e3983d49e0052e94ea4bb6ace8c23d7
ChristyAravind <aravindmanimozhi@gmail.com>
2022-03-26 12:38:08 +0530

    recent email

--- src/components/contactUs.js
+++ src/components/contactUs.js
@@ -33,16 +33,13 @@ export default function ContactUs() {
                 <input
                   class="form-control"
                   id="email"
-                  type="email"
+                  type="text"
                   placeholder="name@example.com"
                   data-sb-validations="required,email"
                 />
                 <label for="email">Email address</label>
                 <div class="invalid-feedback" data-sb-feedback="email:required">
                   An email is required.
-                </div>
-                <div class="invalid-feedback" data-sb-feedback="email:email">
-                  Email is not valid.
                 </div>
               </div>
 
